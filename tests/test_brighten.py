ea9a122f1caeb35f447f7177e1afedf9ab45d36d
shininglight35 <garciaroshan121802@gmail.com>
2026-02-08 19:27:32 +0800

    Update test_brighten.py

--- tests/test_brighten.py
+++ tests/test_brighten.py
@@ -5,7 +5,7 @@ import pytest
 import sys
 from pathlib import Path
 
-sys.path.append(str(Path(__file__).resolve().parents[1]))
+sys.path.append(str(Path(__file__).resolve().parents[1] / "src"))
 
 from brighten import (
     apply_clahe,
